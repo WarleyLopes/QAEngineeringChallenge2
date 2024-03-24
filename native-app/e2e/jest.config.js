2c05a347306502971cf8d0ba98c29a092422fad3
Warley Lopes <warley@5s.technology>
2024-03-24 19:08:00 -0400

    chore: adds more on artifacts for detox e2e tests

--- native-app/e2e/jest.config.js
+++ native-app/e2e/jest.config.js
@@ -1,12 +1,21 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
-  rootDir: "..",
-  testMatch: ["<rootDir>/e2e/**/*.test.js"],
+  rootDir: '..',
+  testMatch: ['<rootDir>/e2e/**/*.test.js'],
   testTimeout: 120000,
   maxWorkers: 1,
-  globalSetup: "detox/runners/jest/globalSetup",
-  globalTeardown: "detox/runners/jest/globalTeardown",
-  reporters: ["detox/runners/jest/reporter"],
-  testEnvironment: "detox/runners/jest/testEnvironment",
-  verbose: true,
+  globalSetup: 'detox/runners/jest/globalSetup',
+  globalTeardown: 'detox/runners/jest/globalTeardown',
+  reporters: [
+    'detox/runners/jest/reporter',
+    [
+      'jest-junit',
+      {
+        outputDirectory: '<rootDir>/artifacts/reports/',
+        outputName: 'test_results.xml'
+      }
+    ]
+  ],
+  testEnvironment: 'detox/runners/jest/testEnvironment',
+  verbose: true
 };
